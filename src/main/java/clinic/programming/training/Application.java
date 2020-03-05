84d23fdbf2cd8e1091404583606db67f0714b762
Suman-05 <suman.sthavaramath@wisseninfotech.com>
2020-03-05 14:53:36 +0530

    adding some files

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,37 +1,7 @@
-package clinic.programming.training;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.commons.lang3.StringUtils;
-
-public class Application {
-
-	public int countwords(String words)
-	{
-	String[] separateWords=StringUtils.split(words,' ');
-	return(separateWords==null)?0 :separateWords.length;
-	}
-
-	public void greet()
-	{
-	List<String>greetings = new ArrayList<>();
-	greetings.add("Hello");
-	for(String greeting : greetings)
-	{
-	System.out.println("Greeting:" + greeting);
-	}
-	}
-    
-    public Application() {
-        System.out.println ("Inside Application");
-    }
-
-    // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
-	app.greet();
-	int count = app.countwords("i have four words");
-	System.out.println("word count: " + count);
-    }
+class Application
+{
+public static void main(String args[])
+{
+System.out.println("Hello World");
+}
 }
